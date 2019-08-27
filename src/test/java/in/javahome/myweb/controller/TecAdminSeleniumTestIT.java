0281e3e87d3f9b4940f487f37d9ce00a9ae7c35e
shifali0102 <52269278+shifali0102@users.noreply.github.com>
2019-08-27 12:14:15 +0530

    Update TecAdminSeleniumTestIT.java

--- src/test/java/in/javahome/myweb/controller/TecAdminSeleniumTestIT.java
+++ src/test/java/in/javahome/myweb/controller/TecAdminSeleniumTestIT.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+/*import java.io.IOException;
  
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -28,3 +28,4 @@ public class TecAdminSeleniumTestIT {
         }
 }
 
+*/
